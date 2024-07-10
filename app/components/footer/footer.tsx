83b1b4879171f058744a39621934b52ff72e4a04
NicoAlvaFrontend <nicoalva.frontend@gmail.com>
2024-07-10 18:20:03 -0300

    tripodysite

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         <div className='footer flex items-center'>
             <p><b className='footer-information'>© Tripody S.A. - Todos los derechos reservados</b></p>
             <p><b className='footer-information'>Mail: tripodysa@gmail.com</b></p>
-            <p><b className='footer-information'>Contact: +54 911 1234-5678</b></p>
+            <p><b className='footer-information'>Contact: +54 911 8765-4321</b></p>
         </div>
     )
 }
